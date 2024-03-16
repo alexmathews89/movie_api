635dea835aa8d32dc29dbbdd605c3efa266dc01e
alexmathews89 <alexmathews@live.com>
2024-03-16 12:49:19 -0700

    Updated CRUD methods to access models

--- index.js
+++ index.js
@@ -3,126 +3,146 @@ const express = require("express"),
   morgan = require("morgan");
 const app = express();
 
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect("mongodb://localhost:27017/cfDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(bodyParser.json());
 
-let topMovies = [
-  {
-    title: "Avatar",
-    director: "James Cameron",
-    genre: "Science Fiction",
-  },
-  {
-    title: "Lord of the Rings",
-    director: "Peter Jackson",
-    genre: "Fantasy",
-  },
-  {
-    title: "Star Wars",
-    director: "George Lucas",
-    genre: "Science Fiction",
-  },
-  {
-    title: "Indiana Jones",
-    director: "Steven Spielberg",
-    genre: "Action",
-  },
-  {
-    title: "I Love You, Man",
-    director: "John Hamburg",
-    genre: "Comedy",
-  },
-  {
-    title: "The Mask of Zorro",
-    director: "Martin Campbell",
-    genre: "Action",
-  },
-  {
-    title: "Aliens",
-    director: "James Cameron",
-    genre: "Science Fiction",
-  },
-  {
-    title: "The Dark Kight",
-    director: "Christopher Nolan",
-    genre: "Action",
-  },
-  {
-    title: "Good Will Hunting",
-    director: "Gus Vant Sant",
-    genre: "Drama",
-  },
-  {
-    title: "Jurassic Park",
-    director: "Steven Spielberg",
-    genre: "Action",
-  },
-];
-
 app.use(morgan("common"));
-
-app.get("/movies", (req, res) => {
-  res.json(topMovies);
-});
 
 app.get("/", (req, res) => {
   res.send("Welcome to myFlix app!");
 });
 
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 app.get("/movies/:Title", (req, res) => {
-  const Title = req.params.Title;
-  const movie = topMovies.find((movie) => movie.title === Title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send("no such movie");
-  }
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-app.get("/movies/genre/:genreName", (req, res) => {
-  const genreName = req.params.genreName;
-  const genre = topMovies.find((movie) => movie.genre === genreName);
-
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send("no such genre");
-  }
-});
-
-app.get("/movies/director/:directorName", (req, res) => {
-  const directorName = req.params.directorName;
-  res.send("Will send info on " + directorName + " as a director");
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.post("/users", (req, res) => {
-  res.send("User Registration Page");
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
-app.put("/users/:username", (req, res) => {
-  const username = req.params.username;
-  res.send("Will allow " + username + " to updated their information");
+app.put("/users/:Username", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-app.put("/users/:username/movies/:movieID", (req, res) => {
-  const username = req.params.username;
-  const movie = req.params.movieID;
-  res.send(
-    "Will allow " + username + " to add a " + movie + "to their favorites"
-  );
+app.post("/users/:Username/movies/:_id", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $addToSet: { FavoriteMovies: req.params._id },
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-app.delete("/users/:username/movies/:movieID", (req, res) => {
-  const username = req.params.username;
-  const movie = req.params.movieID;
-  res.send(
-    "Will allow " + username + " to remove a " + movie + " from their favorites"
-  );
-});
-
-app.delete("/users/:username", (req, res) => {
-  const username = req.params.username;
-  res.send("Will allow " + username + " to remove their information");
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.use(express.static("public"));
