df7109fa6c52f316a59977932644793b3a1a6672
alexmathews89 <alexmathews@live.com>
2024-03-29 20:07:35 -0700

    added fallback connection uri

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ const { check, validationResult } = require("express-validator");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect(process.env.CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.CONNECTION_URI || "mongodb://localhost:27017/cfDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(bodyParser.json());
 
