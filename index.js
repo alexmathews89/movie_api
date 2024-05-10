370641ba419b52ed8ec44161bdd384af460f8742
alexmathews89 <alexmathews@live.com>
2024-05-09 18:50:28 -0700

    changed findOneAndRemove to findOneAndDelete for deleting a user

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ app.delete(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Users.findOneAndRemove({ Username: req.params.Username })
+    Users.findOneAndDelete({ Username: req.params.Username })
       .then((user) => {
         if (!user) {
           res.status(400).send(req.params.Username + " was not found");
